1c78c3171720ca8aa07295f1ec4d99df2bd25487
JekSkeez <98408944+JekSkeez@users.noreply.github.com>
2023-04-15 04:58:29 +0700

    Add files via upload

---
+++ hidemygen1.py
@@ -0,0 +1,33 @@
+import requests
+
+url = 'https://hidemy.name/en/demo/'
+
+if 'Your email' in requests.get(url).text:
+    
+    email = input('Введите электронную почту для получения тестового периода: ')
+
+    response = requests.post('https://hidemy.name/en/demo/success/', data={
+        "demo_mail": f"{email}"
+    })
+
+    if 'Your code has been sent to your' in response.text:
+        print('Код отправлен! Проверьте почту.')
+        
+        while True:
+            try:
+                response = requests.get(confirm)
+                if 'has been sent' in response.text:
+                    print('Почта подтверждена. Код отправлен на вашу почту!')
+                    break
+                else:
+                    confirm = input('Ссылка невалидная, повторите попытку: ')
+            except:
+                confirm = input('Ссылка невалидная, повторите попытку: ')
+                continue
+
+
+    else:
+        print('Указанная почта не подходит для получения тестового периода ')
+
+else:
+    print('Невозможно получить тестовый период')
